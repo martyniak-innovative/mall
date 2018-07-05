d1712be5b973d1a4422cceebf9e7fdeab0ada148
rascada <carftjr@gmail.com>
2018-07-05 03:02:43 +0200

    update public api

--- projects/mall/src/public_api.ts
+++ projects/mall/src/public_api.ts
@@ -2,5 +2,8 @@
  * Public API Surface of mall
  */
 
+export * from './lib/elevator.service';
 export * from './lib/mall.service';
+export * from './lib/elevations';
+export * from './lib/elevation';
 // export * from './lib/mall.module';

1dd99884f2a7989dc63bb18d7fdd89d02f17099e
rascada <carftjr@gmail.com>
2018-08-01 16:11:53 +0200

    refactor mall with consistent inject object

--- projects/mall/src/lib/mall.service.ts
+++ projects/mall/src/lib/mall.service.ts
@@ -9,45 +9,37 @@ import { map } from 'rxjs/operators';
 export class Mall {
   constructor(private db: AngularFirestore) { }
 
-  collection(collection, queryFn?): Observable<any> {
+  collection(collection, queryFn?, inject?): Observable<any> {
     return this.db.collection(collection, ref => this.query(ref, queryFn))
-      .snapshotChanges().pipe(this.extract());
+      .snapshotChanges().pipe(this.extract(inject));
   }
 
-  document(document): Observable<any> {
-    return this.db.doc(document).snapshotChanges()
-      .pipe(map(item => this.extractOne(item.payload)));
+  by(collection, property, value, inject?): Observable<any> {
+    return this.collection(collection, q => q.where(property, '==', value), inject);
   }
 
   add(collection, data): Promise<any> {
     return this.db.collection(collection).add(data);
   }
 
-  by(collection, property, value, inject?): Observable<any> {
-    return this
-      .collection(collection, q => q.where(property, '==', value))
-      .pipe(map(arr => {
-        const doc = arr[0];
-
-        if (doc && inject) {
-          Object.assign(doc, inject);
-        }
-
-        return doc;
-      }));
+  document(document, inject?): Observable<any> {
+    return this.db.doc(document).snapshotChanges()
+      .pipe(
+        map(item => this.extractOne(item.payload, inject),
+    ));
   }
 
   private query(ref, fn) {
     return fn ? fn(ref) : ref;
   }
 
-  private extract() {
+  private extract(inject?) {
     return map((items: any[]) =>
-      items.map(item => this.extractOne(item.payload.doc))
+      items.map(item => this.extractOne(item.payload.doc, inject))
     );
   }
 
-  private extractOne(doc) {
-    return { ...doc.data(), doc };
+  private extractOne(doc, inject?) {
+    return { ...doc.data(), doc, ...inject };
   }
 }

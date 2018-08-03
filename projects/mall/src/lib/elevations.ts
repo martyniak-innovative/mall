c0f9c337392b2342812ece84b8c013e3edf44423
rascada <carftjr@gmail.com>
2018-08-03 13:28:34 +0200

    better items mixing with multi-query pagination

--- projects/mall/src/lib/elevations.ts
+++ projects/mall/src/lib/elevations.ts
@@ -8,37 +8,14 @@ export class Elevations {
   private _elevations: BehaviorSubject<Elevation[]> = new BehaviorSubject([]);
 
   loading: Observable<boolean>;
-  items: Observable<any[]>;
-  done: Observable<boolean>;
   amount: Observable<number>;
+  done: Observable<boolean>;
+  items: Observable<any[]>;
 
   constructor(private elevator: Elevator, elevations: Elevation[]) {
     this._elevations.next(elevations);
 
     this.setup();
-  }
-
-  setup() {
-    this.items = this.combine('items').pipe(
-      map((items: any) => items[0].concat(...items.slice(1))));
-
-    this.loading = this.combine('loading').pipe(
-      map((bools: boolean[]) => bools.find(bool => bool === true) || false));
-
-    this.done = this.combine('done').pipe(
-      map(bools => bools.every(bool => bool === true) || false));
-
-    this.amount = this.combine('done').pipe(
-      map((bools: boolean[]) => bools.reduce((prev, bool) => prev + (bool ? 1 : 0), 0))
-    );
-  }
-
-  private combine(prop) {
-    return this._elevations.asObservable().pipe(
-      switchMap(elevations =>
-        combineLatest(...elevations.map(ev => ev[prop]))
-      )
-    );
   }
 
   more() {
@@ -57,5 +34,49 @@ export class Elevations {
     });
 
     this._elevations.next(elevations);
+  }
+
+  private combine(prop) {
+    return this._elevations.asObservable().pipe(
+      switchMap(elevations =>
+        combineLatest(...elevations.map(ev => ev[prop]))
+      )
+    );
+  }
+
+  private setup() {
+    this.items = this.combine('items')
+      .pipe(map(items => this.mixItems(items)));
+
+    this.loading = this.combine('loading').pipe(
+      map((bools: boolean[]) => bools.find(bool => bool === true) || false));
+
+    this.done = this.combine('done').pipe(
+      map(bools => bools.every(bool => bool === true) || false));
+
+    this.amount = this.combine('done').pipe(
+      map((bools: boolean[]) => bools.reduce((prev, bool) => prev + (bool ? 1 : 0), 0))
+    );
+  }
+
+  private mixItems(items) {
+    let _items = [];
+    let length = 0;
+
+    items.forEach(item => {
+      if (item.length > length) {
+        length = item.length;
+      }
+    });
+
+    for (let i = 0; i < length; i++) {
+      items.forEach(item => {
+        if (item[i]) {
+          _items = [..._items, item[i] ];
+        }
+      });
+    }
+
+    return _items;
   }
 }
